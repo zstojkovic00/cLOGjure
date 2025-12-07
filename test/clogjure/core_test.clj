45db41ee1cc2ec3b23993b07f2d0610ead04aab9
zstojkovic00 <00zeljkostojkovic@gmail.com>
2025-12-07 20:10:02 +0100

    ignore spark1GB.log file

---
+++ test/clogjure/core_test.clj
@@ -0,0 +1,7 @@
+(ns clogjure.core-test
+  (:require [clojure.test :refer :all]
+            [clogjure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
--- test/first_project/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns first-project.core-test
-  (:require [clojure.test :refer :all]
-            [first-project.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
