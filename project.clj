029a3984624f39bf246c2add94cd6728f5ab67bd
zstojkovic00 <00zeljkostojkovic@gmail.com>
2025-12-23 13:16:14 +0100

    push it to the limit

--- project.clj
+++ project.clj
@@ -2,6 +2,15 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.12.3"]
+                 [org.uncomplicate/neanderthal-base "0.60.0"]
+                 [org.uncomplicate/neanderthal-mkl "0.60.0"]
+                 [org.bytedeco/mkl "2025.2-1.5.12" :classifier "linux-x86_64-redist"]]
+
+  :repositories [["maven-central-snapshots" "https://central.sonatype.com/repository/maven-snapshots"]]
+
+  :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"
+                       "--enable-native-access=ALL-UNNAMED"]
+
   :repl-options {:init-ns first-project.core})
